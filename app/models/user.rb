795e8bc944e78b759314a2afba14204788bf735c
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-25 00:16:58 +0900

    テストコード全角カナの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
-   validates :last_name
-   validates :first_name
-   validates :last_name_kana
-   validates :first_name_kana
- end
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+  validates :last_name
+  validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'は全角カタカナで入力して下さい。' } do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
 
   validates :nickname, presence: true
   validates :last_name, presence: true
@@ -17,6 +20,6 @@ class User < ApplicationRecord
   validates :birthday, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {"山田"}
-    first_name            {"太郎"}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { '太郎' }
     birthday              { 2020 }
-    last_name_kana        { "やまだ" }
-    first_name_kana       { "たろう" }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
   end
 end
