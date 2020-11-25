69f0a78ce489eaf29e0209dad4d4aee95b0d7c05
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-25 11:29:16 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,9 @@ class User < ApplicationRecord
   validates :last_name_kana
   validates :first_name_kana
   end
-
+  
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
   validates :birthday, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
