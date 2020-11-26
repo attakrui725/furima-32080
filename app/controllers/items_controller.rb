98b761ae08f4f06dd2717ec4353099a7971d4e4b
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-26 16:29:09 +0900

    利益計算機能の導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,30 @@
 class ItemsController < ApplicationController
+before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
+  @item = Item.new
+
   end
+
+  def create
+  @item = Item.new(item_params)
+
+  if @item.save
+    redirect_to index
+  else
+    render :new
+  end
+
+  end
+
+private
+
+def item_params
+  params.require(:item).permit(:image,:name, :description, :price,:category_id, :status_id, :delivery_fee_id, :shipment_source_id, :day_to_ship_id)
+end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :name, presence: true               
+  validates :description, presence: true        
+  validates :category_id, numericality: { other_than: 1 }         
+  validates :status_id, numericality: { other_than: 1 }           
+  validates :delivery_fee_id, numericality: { other_than: 1 }     
+  validates :shipment_source_id, numericality: { other_than: 1 } 
+  validates :day_to_ship_id, numericality: { other_than: 1 }   
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'に半角数字を使用してください' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999  }     
+
+  
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre, :status, :delivery_fee, :day_to_ship, :shipment_source
 end
