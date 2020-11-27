414d1998e7c9cef0e42ee02db1393abcc19ad2a1
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-27 11:08:46 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,14 @@
 class Item < ApplicationRecord
   validates :image, presence: true
-  validates :name, presence: true               
-  validates :description, presence: true        
-  validates :category_id, numericality: { other_than: 1 }         
-  validates :status_id, numericality: { other_than: 1 }           
-  validates :delivery_fee_id, numericality: { other_than: 1 }     
-  validates :shipment_source_id, numericality: { other_than: 1 } 
-  validates :day_to_ship_id, numericality: { other_than: 1 }   
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'に半角数字を使用してください' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999  }     
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :shipment_source_id, numericality: { other_than: 1 }
+  validates :day_to_ship_id, numericality: { other_than: 1 }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'に半角数字を使用してください' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999 }
 
-  
   belongs_to :user
   has_one_attached :image
 
