3eace1a9129b85599d2bf64dc36a5914f811bffb
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-27 14:52:39 +0900

    テストコード_with_options修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
+  with_options presence: true do 
+  validates :image 
+  validates :name
+  validates :description
+  end
   
   with_options numericality: { other_than: 1 } do
   validates :category_id
@@ -10,6 +12,7 @@ class Item < ApplicationRecord
   validates :shipment_source_id
   validates :day_to_ship_id
   end
+  
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'に半角数字を使用してください' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999_999 }
 
   belongs_to :user
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
-      it '価格が10000000000以上ではでは保存できない' do
+      it '価格が10000000000以上では保存できない' do
         @item.price = 10000000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999999")
