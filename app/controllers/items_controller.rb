4cc4d292c1cd5aef4f91cf0c96635ebb67136233
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-30 14:18:15 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,16 +37,16 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   item = Item.find(params[:id])
+  def destroy
+    item = Item.find(params[:id])
 
-  #   if item.destroy
-  #     redirect_to root_path
+    if item.destroy
+      redirect_to root_path
 
-  #   else
-  #     render :show
-  #   end
-  # end
+    else
+      render :show
+    end
+  end
 
   private
 
